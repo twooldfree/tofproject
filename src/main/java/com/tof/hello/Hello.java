9d138e74e0a023981f2d05b788982b43f3edcd65
wenyunlong <362439839@qq.com>
2016-07-01 11:45:01 +0800

    第一次提交  集成maven

---
+++ src/main/java/com/tof/hello/Hello.java
@@ -0,0 +1,14 @@
+package com.tof.hello;
+
+/**
+ * Created by wen on 2016/7/1.
+ */
+public class Hello {
+    public void sayHello(){
+        System.out.println("¹þÆ¤");
+    }
+
+    public static void main(String[] args){
+        new Hello().sayHello();
+    }
+}
